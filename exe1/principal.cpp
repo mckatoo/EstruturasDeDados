2b0c9decbe1970ebe0c60e9bd6b6a2f697fc5a74
Milton Carlos Katoo <mckatoo@gmail.com>
2019-08-26 19:10:46 -0300

    ASFDSDF

--- exe1/principal.cpp
+++ exe1/principal.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int nome[10];
+    string nome[10];
+    string nome_para_pesquisar, resultado_da_pesquisa = "NÃO ACHEI.\n";
     for (int i = 0; i < 10; i++)
     {
         cout << "INFORME O NOME DA " << i + 1 << " PESSOA.\n";
+        cin >> nome[i];
     }
+
+    cout << "INFORME UM NOME PARA PESQUISAR.\n";
+    cin >> nome_para_pesquisar;
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (nome[i] == nome_para_pesquisar)
+        {
+            resultado_da_pesquisa = "ACHEI\n";
+        }
+    }
+
+    cout << resultado_da_pesquisa;
 
     return 0;
 }
