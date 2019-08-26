af932c4335908baf80ab6ffb78842fa644cd7612
Milton Carlos Katoo <mckatoo@gmail.com>
2019-08-25 22:00:58 -0300

    ADD EXERCICIOS

---
+++ exe1/principal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int nome[10];
+    for (int i = 0; i < 10; i++)
+    {
+    cout<<"INFORME O NOME DA " << i+1 << " PESSOA.";
+    }
+    
+    return 0;
+}
